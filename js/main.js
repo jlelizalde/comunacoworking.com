1af5f2e23e84e7ea5c5a6c5a12ccbbc3f5f9e505
jlelizalde <jler.152@gmail.com>
2016-07-31 14:47:28 -0500

    Se añaden animaciones, se añade correctamente el envío de formulario y cambios en estilos

--- js/main.js
+++ js/main.js
@@ -1,13 +1,50 @@
-$(document).ready(function() {
-    // $('#fullpage').fullpage({
-    //     autoScrolling : false,
-    //     fitToSection : false,
-    //     anchors : [
-    //         'data-header','data-primer-comentario','data-servicios',
-    //         'data-segundo-comentario','data-membresias','data-tercer-comentario',
-    //         'data-rentas',
-    //         // 'data-cuarto-comentario',
-    //         'data-contacto','data-footer'
-    //     ]
-    // });
+// from https://css-tricks.com/snippets/jquery/smooth-scrolling/
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+$.fn.extend({
+    animateCss: function (animationName) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+        });
+    }
+});
+
+$(function() {
+    $('#job-opennings').click(function(event) {
+        event.preventDefault();
+        $(this).animateCss('wobble');
+        swal('Pronto tendremos más noticias');
+    });
+
+    $contactForm = $('#contact-form');
+    $contactForm.submit(function(event) {
+        event.preventDefault();
+        $.ajax({
+            url: '//formspree.io/comuna.coworking@gmail.com',
+            method: 'POST',
+            data: $(this).serialize(),
+            dataType: 'json',
+            success: function(data) {
+                swal("¡Gracias por tu interés!", "En breve nos comunicaremos contigo!", "success")
+            },
+            error: function(err) {
+                swal("Algo salió mal", ":(", "error")
+            }
+        });
+     
+    });
 });
